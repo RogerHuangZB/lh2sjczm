394ba27f00f7b95136e02f922b11e5d184d31f45
huangzb <huangzb@cares.sh.cn>
2017-08-07 06:47:46 +0800

    login

--- trunk/20project/20.40source/jc_web/static/js/register.js
+++ trunk/20project/20.40source/jc_web/static/js/register.js
@@ -1,4 +1,13 @@
 $(function() {
+    $('#userName').blur(function () {
+        var userName = $('#userName').val();
+        if(isUserNameExist(userName)){
+            console.log("用户名已存在!");
+        }else{
+            console.log("用户名可以使用！");
+        }
+    });
+    
     $('#registerBtn').click(function () {
         var userName = $('#userName').val();
         var password = $('#password').val();
@@ -6,6 +15,11 @@ $(function() {
         if (userName === ""){
             console.log("请输入用户名！");
             return;
+        }
+        if(isUserNameExist(userName)){
+            console.log("用户名已存在!");
+        }else{
+            console.log("用户名可以使用！");
         }
         if (password === ""){
             console.log("请输入密码！");
@@ -20,11 +34,19 @@ $(function() {
         } else if(pwdCheck !== password){
             console.log("两次密码输入不一致！");
             return;
-        }
+        }//验证是否有除大小写字母数字以外的特殊字符
+        //验证是否有字母
+        //验证是否有数字
         register(userName, password);
     });
 });
 
 function register(u, p) {
+    console.log("注册成功！");
     window.location = "../index.html";
+}
+
+function isUserNameExist(u) {
+    console.log(u);
+    return false;
 }
