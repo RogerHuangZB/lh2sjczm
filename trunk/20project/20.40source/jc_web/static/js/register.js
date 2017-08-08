aa91f444ed4614c70938448c3522cdfa59b64e5b
huangzb <huangzb@cares.sh.cn>
2017-08-08 23:56:48 +0800

    login

--- trunk/20project/20.40source/jc_web/static/js/register.js
+++ trunk/20project/20.40source/jc_web/static/js/register.js
@@ -1,10 +1,8 @@
 $(function() {
     $('#userName').blur(function () {
         var userName = $('#userName').val();
-        if(isUserNameExist(userName)){
-            console.log("用户名已存在!");
-        }else{
-            console.log("用户名可以使用！");
+        if (userName !== ""){
+            isUserNameExist(userName);
         }
     });
     
@@ -13,26 +11,26 @@ $(function() {
         var password = $('#password').val();
         var pwdCheck = $('#pwdCheck').val();
         if (userName === ""){
-            console.log("请输入用户名！");
+            alert("请输入用户名！");
             return;
         }
         if(isUserNameExist(userName)){
-            console.log("用户名已存在!");
+            alert("用户名已存在!");
         }else{
             console.log("用户名可以使用！");
         }
         if (password === ""){
-            console.log("请输入密码！");
+            alert("请输入密码！");
             return;
         } else if(password.length < 6){
-            console.log("密码长度不足！");
+            alert("密码长度不足！");
             return;
         }
         if (pwdCheck === ""){
-            console.log("请再次输入密码！");
+            alert("请再次输入密码！");
             return;
         } else if(pwdCheck !== password){
-            console.log("两次密码输入不一致！");
+            alert("两次密码输入不一致！");
             return;
         }//验证是否有除大小写字母数字以外的特殊字符
         //验证是否有字母
@@ -42,11 +40,33 @@ $(function() {
 });
 
 function register(u, p) {
-    console.log("注册成功！");
+    alert("注册成功！");
     window.location = "../index.html";
 }
 
 function isUserNameExist(u) {
-    console.log(u);
-    return false;
+    $.ajax({
+        type: 'POST',
+        url: 'http://192.168.0.102:9090/user/isUserExist',
+        dataType: "json",
+        async: false,
+        headers:{
+            authorization:"tokend652c36290024c0f8ee99568c8c1daf0"
+        },
+        data: {loginName:u},
+        success: function (data) {
+            console.log(data);
+            if(data.code !== 'ok'){
+                alert("用户名已存在!");
+            }else{
+                console.log("用户名可以使用！");
+            }
+        },
+        error:  function(XMLHttpRequest, textStatus, errorThrown){
+            //通常情况下textStatus和errorThrown只有其中一个包含信息
+            console.log(XMLHttpRequest);
+            console.log(textStatus);
+            console.log(errorThrown);
+        }
+    });
 }
--- trunk/20project/20.40source/jc_web/static/js/sign_in.js
+++ trunk/20project/20.40source/jc_web/static/js/sign_in.js
@@ -7,14 +7,14 @@ $(function() {
         var userName = $('#userName').val();
         var password = $('#password').val();
         if (userName === ""){
-            console.log("请输入用户名！");
+            alert("请输入用户名！");
             return;
         }
         if (password === ""){
-            console.log("请输入密码！");
+            alert("请输入密码！");
             return;
         } else if(password.length < 6){
-            console.log("密码长度不对！");
+            alert("密码长度不对！");
             return;
         }
         login(userName, password);
@@ -28,13 +28,13 @@ function login(u,p) {
     };
     $.ajax({
         type: 'POST',
-        url: 'http://192.168.0.101:9090/login',
+        url: 'http://192.168.0.102:9090/login',
         contentType: "application/json",
         dataType: "json",
         data: JSON.stringify(data),
         success: function (data) {
             console.log(data);
-
+            window.location = "../index.html";
         },
         error:  function(XMLHttpRequest, textStatus, errorThrown){
             //通常情况下textStatus和errorThrown只有其中一个包含信息
